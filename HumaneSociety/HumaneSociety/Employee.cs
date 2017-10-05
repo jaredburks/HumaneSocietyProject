6aca295ac34f44168a73c3dbe2a90e38537d66b6
jaredburks <jarabu93@gmail.com>
2017-10-05 15:42:53 -0500

    Added employee menu and prompt.

--- HumaneSociety/HumaneSociety/Employee.cs
+++ HumaneSociety/HumaneSociety/Employee.cs
@@ -10,8 +10,29 @@ namespace HumaneSociety
     {
         Animal_Info animal = new Animal_Info();
 
+        public void EmployeePrompt()
+        {
+            Console.WriteLine("Employee Menu\nENTER '1' to search for an animal, '2' to add an animal to database.");
+        }
         //Method for employee menu 
+        public void EmployeeMenu()
+        {
+            EmployeePrompt();
+            string option;
+            switch(option = Console.ReadLine())
+            {
+                case "1"://For Searching for an animal
 
+                    break;
+                case "2"://For Adding an animal to database
+                    AddAnimal();
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid option.\n");
+                    EmployeeMenu();
+                    break;
+            }
+        }
         //Method to Add an animal to the database
         public void AddAnimal()
         {
