b275f9c9ce8393148577db5ea4202a7aa1cd0a62
jaredburks <jarabu93@gmail.com>
2017-10-09 09:07:07 -0500

    Started search by traits method and finished prompt for adopter menu.

--- HumaneSociety/HumaneSociety/Adopter.cs
+++ HumaneSociety/HumaneSociety/Adopter.cs
@@ -8,11 +8,37 @@ namespace HumaneSociety
 {
     class Adopter
     {
+        //Prompt for Adopter
+        public void AdopterPrompt()
+        {
+            Console.WriteLine("Welcome to the Adopter menu.");
+            Console.WriteLine("Enter '1' to search for animal by traits, '2' to create a personal info profile, '3' to return to Portal menu.");
+        }
         //Method for menu
-
+  
         //Method for creating a table to share personal info
 
-        //Method to search for an animal
-
+        //Method to search for an animal by traits
+        public void SearchByTraits()
+        {
+            string option;
+            Console.WriteLine("Enter '1' to search by type of animal, '2' by age, '3' for animals that have shots, '4' to return to Adopter menu.");
+            switch (option = Console.ReadLine())
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid option.");
+                    SearchByTraits();
+                    break;
+            }
+        }
     }
 }
